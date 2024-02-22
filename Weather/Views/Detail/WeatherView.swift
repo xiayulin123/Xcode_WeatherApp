c6f68e4cb195979ab453fc963ad2b803d174cfaf
xiayulin123 <xiayulinsky@gmail.com>
2024-02-21 19:31:01 -0500

    finished wait for editing the read me file

--- Weather/Views/Detail/WeatherView.swift
+++ Weather/Views/Detail/WeatherView.swift
@@ -8,23 +8,50 @@
 import SwiftUI
 
 struct WeatherView: View {
+    @State private var searchText = ""
+    
+    var searchResults: [Forecast]{
+        if searchText.isEmpty {
+            return Forecast.cities
+        } else {
+            return Forecast.cities.filter{
+                $0.location.contains(searchText)
+            }
+        }
+    }
     var body: some View {
         ZStack {
             // MARK: Background
             Color.background
                 .ignoresSafeArea()
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(searchResults) {forecast in
+                        WeatherWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
         }
         .overlay {
             // MARK: Navigation Bar
-            NavigationBar()
+            NavigationBar(searchText: $searchText)
         }
         .navigationBarHidden(true)
+//        .searchable(text: $searchText, placement:
+//                .navigationBarDrawer(displayMode: .always), prompt: "Search for a city")
     }
 }
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView()
-            .preferredColorScheme(.dark)
+        NavigationView {
+            WeatherView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
